287b2145de569d9548e9c07822013dc7c6194ff7
Sarvesh singh <sarveshadhikari905@gmail.com>
2025-12-05 11:03:27 +0530

    section 2

---
+++ section2/array-intro.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a[5] = {2,3,4,5,7};  // initialization
+    cout<<"value at index 2 is "<<a[2]<<endl;    // printing values
+
+    int arr[10] = {1,2,3,4};
+    for(int i=0;i<10;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+
+    int ar[10];
+    for(int i=0;i<10;i++){
+        cout<<ar[i]<<" "; // prints garbage values
+    }
+    cout<<endl;
+
+    int b[10];
+    for(int i=0;i<10;i++){
+        b[i]= i*i;  // given values to all the index in the array
+        cout<<b[i]<<" ";
+    }
+
+    return 0;
+}   
