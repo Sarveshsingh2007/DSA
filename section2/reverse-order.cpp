cf8666c21755b083ff4fbacf99908db1a49ec885
Sarvesh singh <sarveshadhikari905@gmail.com>
2025-12-09 10:02:13 +0530

    reverse-order

---
+++ section2/reverse-order.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    int n;
+    cout<<"Enter the size of array: ";
+    cin>>n;
+    
+    int arr[n];
+    cout<<"Size of an array is: "<<sizeof(arr)<<endl;
+    cout<<"Enter elements: \n";
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    cout<<endl;
+
+    cout<<"Original Array: ";
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+
+    cout<<"Reverse Array: ";
+    for(int i=n-1;i>=0;i--){
+        cout<<arr[i]<<" ";
+    }
+
+    return 0;
+}
