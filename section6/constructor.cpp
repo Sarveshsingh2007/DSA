499dd60797716b9c7294296cc5a8bf42619f3c41
Sarvesh singh <sarveshadhikari905@gmail.com>
2026-01-07 12:47:12 +0530

    constructors default and parameterized

---
+++ section6/constructor.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include "constructorclass.cpp"
+
+using namespace std;
+
+int main(){
+
+    cout<<"Default Constructor Demo: "<<endl;
+    Student s1;
+    s1.display();
+    cout<<endl;
+    Student s2;
+    s2.display();
+
+    cout<<endl;
+    cout<<endl;
+
+    cout<<"Parameterized Constructor Demo: "<<endl;
+    Student s3(10);
+    s3.display();
+    cout<<endl;
+
+    return 0;
+}    
