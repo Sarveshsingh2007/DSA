6bfb8ba85d7743c0f6573807d4ddabb2cb3d43be
Sarvesh singh <sarveshadhikari905@gmail.com>
2026-01-07 11:05:02 +0530

    updated the studentclass

--- section6/studentclass.cpp
+++ section6/studentclass.cpp
@@ -1,12 +1,29 @@
+#include<iostream>
 #include <string>
 using namespace std;
 
 class Student {
-public:
-    int age;
-    int roll_no;
-    string name;
+    public:
+        int age;
+        int roll_no;
+        string name;
 
-private:
-    int id;
+    private:
+        int id;
+    
+    public:
+        void display(){
+            cout<<"age is: "<<age<<endl;
+            cout<<"roll_no is: "<<roll_no<<endl;
+            cout<<"name is: "<<name<<endl;
+            cout<<"id is: "<<id<<endl;
+        }
+
+        int getid(){
+            return id;
+        }
+        void setid(int idstd){
+            id = idstd;
+        }
+
 };
