24f3880492c5678d1668978916e0609bf87e8bcb
Sarvesh singh <sarveshadhikari905@gmail.com>
2026-01-08 19:29:29 +0530

    file containing class for copy constructor

---
+++ section6/copyconstructorclass.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+ 
+class Employee{
+    int id;
+public:    
+    int age;
+    Employee(int id, int age){
+        cout<<"parameterized constructor called"<<endl;
+        this->id = id;
+        this->age = age;
+    }
+    void display(){
+        cout<<"id is: "<<id<<endl;
+        cout<<"age is: "<<age<<endl;
+    }
+};
