83ad098df83f90cf44c1124a360eef63569ab031
Sarvesh singh <sarveshadhikari905@gmail.com>
2026-02-17 09:59:45 +0530

    check if the array is sorted or not using recursion

---
+++ section12(recursion2-intermidiate)/check-array-sorted.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool isSorted(int a[], int n){
+    // base case 
+    if(n==0 || n==1) return true;
+    if(a[0] > a[1]) return false;
+    
+    // recursive case 
+    bool isSmallerSorted = isSorted(a+1, n-1);
+
+    /*if(isSmallerSorted){
+        return true;
+    }else{
+        return false;
+    }*/
+
+    // calculation
+    return isSmallerSorted;
+}
+
+int main(){
+    int a[] = {1,2,3,4,5};
+    if(isSorted(a,5)) {
+        cout<<"Sorted"<<endl;
+    }else{
+        cout<<"Not Sorted"<<endl;
+    }
+    return 0;
+}
