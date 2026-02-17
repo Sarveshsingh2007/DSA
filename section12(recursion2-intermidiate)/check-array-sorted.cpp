78c2723fc1a885b7f89299f8feb97b802cd487f3
Sarvesh singh <sarveshadhikari905@gmail.com>
2026-02-17 20:07:50 +0530

    modified

--- section12(recursion2-intermidiate)/check-array-sorted.cpp
+++ section12(recursion2-intermidiate)/check-array-sorted.cpp
@@ -19,6 +19,21 @@ bool isSorted(int a[], int n){
     return isSmallerSorted;
 }
 
+bool isSorted2(int a[], int n){
+    // base case 
+    if(n==0 || n==1) return true;
+
+    bool isSmallerSorted = isSorted(a+1, n-1);  
+    if(!isSmallerSorted){
+        return false;
+    }
+    if(a[0] > a[1]){
+        return false;
+    }else{
+        return true;
+    }
+}
+
 int main(){
     int a[] = {1,2,3,4,5};
     if(isSorted(a,5)) {
