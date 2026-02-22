5cff36e3e2c011fb3d7410aedef252a56fe968e0
Sarvesh singh <sarveshadhikari905@gmail.com>
2026-02-22 21:31:08 +0530

    store all position of elements

---
+++ section12(recursion2-intermidiate)/store-all-positions.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void saveAllPos(int a[], int n, int x, int i, vector<int> &ans){
+    if(i==n) return;
+    if(a[i] == x){
+        ans.push_back(i);
+    }
+    saveAllPos(a,n, x,i+1, ans);
+}
+
+int main(){
+    int a[] = {5,5,6,5,6,7};
+    vector<int> v;
+    saveAllPos(a, 6, 5, 0, v);
+    for(int i=0;i<v.size();i++){
+        cout<<v[i]<<" ";
+    }
+}
