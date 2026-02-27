6bb036b87ab1a829188f7ad19fe3899453ae3891
Sarvesh singh <sarveshadhikari905@gmail.com>
2026-02-27 23:48:43 +0530

    updated tower of hanoi--- print steps

--- section13(recursion3-advanced)/tower-of-hanoi.cpp
+++ section13(recursion3-advanced)/tower-of-hanoi.cpp
@@ -8,9 +8,17 @@ int toh(int n){
     return toh(n-1) + 1 + toh(n-2);
 }
 
+void printSteps(int n, char s, char d, char h){
+    if(n==0) return;
+    printSteps(n-1,s,h,d);
+    cout<<"Moving disk "<<n<<" from "<<s<<" to "<<d<<endl;  
+    printSteps(n-1,h,d,s);
+}
+
 int main(){
     int n;
     cin>>n;
     cout<<toh(n)<<endl;
+    print(n, 'A', 'B', 'C');
     return 0;
 }
