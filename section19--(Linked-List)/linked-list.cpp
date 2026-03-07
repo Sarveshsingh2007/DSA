7026d3be911d2a4aac4b833fecf2a7b1e4afcbcb
Sarvesh singh <sarveshadhikari905@gmail.com>
2026-03-07 20:26:14 +0530

    deleted

---
+++ section19--(Linked-List)/linked-list.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+    public:
+        int data;
+        Node *next;
+
+        Node(int data){
+            this->data = data;
+            next = NULL;
+        }
+};
+
+int main(){
+    // static object
+    Node n1(20);
+    Node n2(30);
+    n1.next = &n2;
+    cout<<n1.data<<" "<<n2.data<<endl;
+    Node *head = &n1;
+    cout<<head->data<<endl;
+
+    return 0;
+}
