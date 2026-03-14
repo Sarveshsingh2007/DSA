9499fd9c6dec7f24a74476b6663268cfa3ee1992
Sarvesh singh <sarveshadhikari905@gmail.com>
2026-03-14 23:57:53 +0530

    print ith node

---
+++ section19--(Linked-List)/print-i-th-node.cpp
@@ -0,0 +1,75 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node * next;
+    Node(int data){
+        this->data = data;
+        next = NULL;
+    }
+};
+
+void print(Node *head){
+    Node *temp = head;
+    while(temp != NULL){
+        cout<<temp->data<<"->";
+        temp = temp->next;
+    }
+    cout<<"NULL"<<endl;
+}
+
+int length(Node *head){
+    int count = 0;
+    while(head){
+        count++;
+        head = head->next;
+    }
+    return count;
+}
+void printIthNode(Node *head, int i){
+      if(i<0){
+        cout<<"-1"<<endl;
+        return;
+      }
+      int count = 1;
+      while(count<=i && head!=NULL){
+        head = head->next;
+        count++;
+      }
+      if(head){
+        cout<<head->data<<endl;
+      }else{
+        cout<<"-1"<<endl;
+      }
+}
+Node* takingInput(){
+    int data;
+    cin>>data;
+    Node *head = NULL;  
+    Node *tail = NULL;
+    
+    while(data!= -1){
+        Node *n = new Node(data);
+        if(head == NULL){
+            head = n;
+            tail = n;
+        }else{
+            tail->next = n;
+            tail = n;
+        }
+        cin>>data;
+    }
+    return head;
+}
+
+int main(){
+    Node *head = takingInput();
+    print(head);
+    cout<<"Length: "<<length(head)<<endl;
+    int i;
+    cin>>i;
+    printIthNode(head,i);
+    return 0;
+}
