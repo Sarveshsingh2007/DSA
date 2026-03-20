1185c593ed36df95e52e25006da00bfbbe9a9235
Sarvesh singh <sarveshadhikari905@gmail.com>
2026-03-20 22:09:33 +0530

    length of linked list using recursion

---
+++ section20--(Linked-List--02)/length-recursive.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* next;
+    Node(int data){
+        this->data = data;
+        next = NULL;
+    }
+};
+
+void print(Node *head){
+    Node *temp = head;
+    while(temp!=NULL){
+        cout<<temp->data<<"->";
+        temp = temp->next;
+    }
+    cout<<"NULL";
+}
+
+int length(Node *head){
+    if(head == NULL) return 0;
+    return 1 + length(head->next);
+}
+
+Node* takeInput(){
+    int data;
+    cin>>data;
+    Node *head = NULL;
+    Node *tail = NULL;
+
+    while(data!=-1){
+        Node *n = new Node(data);
+        if(head == NULL){
+            head = n;
+            tail = n;
+        }else{
+            tail->next = n;
+            tail = n;
+        }
+        cin>>data;
+    }
+    return head;
+}
+
+int main(){
+    Node *head = takeInput();
+    print(head);
+    cout<<endl;
+    cout<<"length of linked list is: "<<length(head)<<endl;
+    return 0;
+}
