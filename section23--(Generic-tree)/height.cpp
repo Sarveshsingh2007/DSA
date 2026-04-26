76e72a4da939b465cfbd3a8cf3658017c776eb63
Sarvesh singh <sarveshadhikari905@gmail.com>
2026-04-26 13:20:45 +0530

    find height of tree

---
+++ section23--(Generic-tree)/height.cpp
@@ -0,0 +1,87 @@
+#include<iostream>
+#include<queue>
+#include<vector>
+using namespace std;
+
+template<typename T>
+class TreeNode{
+    public:
+        T data;
+        vector<TreeNode<T>*> children;
+
+        TreeNode(T data){
+            this->data = data;
+        }
+};
+
+TreeNode<int>* takeInputLevelWise(){
+    int rootData;
+    cout<<"Enter root data: ";
+    cin>>rootData;
+
+    TreeNode<int>* root = new TreeNode<int>(rootData);
+    queue<TreeNode<int>*> q;
+    q.push(root);
+
+    while(!q.empty()){
+        TreeNode<int>* f = q.front();
+        q.pop();
+
+        int n;
+        cout<<"Enter the no of children of "<<f->data<<endl;
+        cin>>n;
+
+        for(int i=1;i<=n;i++){
+            int childData;
+            cout<<"Enter "<<i<<" th child of "<<f->data<<": ";
+            cin>>childData;
+
+            TreeNode<int>* child = new TreeNode<int>(childData);
+            q.push(child);
+            f->children.push_back(child);
+        }
+    }
+    return root;
+}
+
+void printTreeLevelWise(TreeNode<int>* root){
+    if(root == NULL) return;
+
+    queue<TreeNode<int>*> q;
+    q.push(root);
+
+    while(!q.empty()){
+        TreeNode<int>* f = q.front();
+        q.pop();
+
+        cout << f->data << " : ";
+        for(int i=0;i<f->children.size();i++){
+            cout<<f->children[i]->data<<" ";
+            q.push(f->children[i]);
+        }
+        cout<<endl;
+    }
+}
+
+int height(TreeNode<int>* root){
+    int max = 0;
+    for(int i=0;i<root->children.size();i++){
+        int childheight = height(root->children[i]);
+        if(childheight > max){
+            max = childheight;
+        }
+    }
+    return max + 1;
+}
+
+int main(){
+    TreeNode<int>* root = takeInputLevelWise();
+
+    cout<<"Height: "<<height(root)<<endl;
+    cout<<endl;
+    cout<<"Tree:"<<endl;
+
+    printTreeLevelWise(root);
+
+    return 0;
+}
