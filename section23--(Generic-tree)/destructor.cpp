8483918af5aa84f0247aaa2540bd2aa79427936a
Sarvesh singh <sarveshadhikari905@gmail.com>
2026-04-27 11:38:28 +0530

    delete tree using destructor

---
+++ section23--(Generic-tree)/destructor.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+using namespace std;
+template<typename T>
+
+class TreeNode{
+    public:
+    T data;
+    vector<TreeNode<T>*> children;
+    TreeNode(T data){
+        this->data = data;
+    }
+    ~TreeNode(){
+        for(int i=0;i<children.size();i++){
+            delete children[i];
+        }
+    }
+};
+
+TreeNode<int>* takeInputLevelWise(){
+    int rootData;
+    cout<<"Enter Root Data: ";
+    cin>>rootData;
+    TreeNode<int>* root = new TreeNode<int>(rootData);
+    queue<TreeNode<int>*> q;
+    q.push(root);
+
+    while(!q.empty()){
+        TreeNode<int>* f = q.front();
+        q.pop();
+        int n;
+        cout<<"Enter total no of childrens of "<<f->data<<": ";
+        cin>>n;
+        for(int i=0;i<n;i++){
+            int childData;
+            cout<<"Enter "<<i+1<<"th child: ";
+            cin>>childData;
+            TreeNode<int>* child = new TreeNode<int>(childData);
+            q.push(child);
+            f->children.push_back(child);
+        }
+    }
+    return root;
+}
+
+// void deleteTree(TreeNode<int>* root){
+//     if(root== NULL) return;
+//     for(int i=0;i<root->children.size();i++){
+//         deleteTree(root->children[i]);
+//     }
+//     delete root;
+// }
+
+int main(){
+    TreeNode<int>* root = takeInputLevelWise();
+    cout<<endl;
+    delete(root);
+    cout<<"Tree Deleted";
+    return 0;
+}
