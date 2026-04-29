31fab04b2c42294e90d3fe29381c5cffc5fd4d3b
Sarvesh singh <sarveshadhikari905@gmail.com>
2026-04-29 23:56:04 +0530

    inorder traversal

---
+++ section24--(Binary-Tree)/InOrder-traversal.cpp
@@ -0,0 +1,72 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+template<typename T>
+
+class BTNode{
+    public:
+    T data;
+    BTNode* left;
+    BTNode* right;
+    BTNode(T data){
+        this->data = data;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+BTNode<int>* takeInputLevelWise(){
+    int rootData;
+    cout<<"Enter Data: ";
+    cin>>rootData;
+    if (rootData == -1) return NULL;
+    BTNode<int>* root = new BTNode<int>(rootData);
+    queue<BTNode<int>*> q;
+    q.push(root);
+    while(!q.empty()){
+        BTNode<int>* f = q.front();
+        q.pop();
+        int leftChildData, rightChildData;
+        cout<<"Enter left child of "<<f->data<<": ";
+        cin>>leftChildData;
+        if(leftChildData!=-1){
+            BTNode<int> * leftChild = new BTNode<int>(leftChildData);
+            f->left = leftChild;
+            q.push(leftChild);
+        }
+        cout<<"Enter right child of "<<f->data<<": ";
+        cin>>rightChildData;
+        if(rightChildData!=-1){
+            BTNode<int> * rightChild = new BTNode<int>(rightChildData);
+            f->right = rightChild;
+            q.push(rightChild);
+        }
+    }
+    return root;
+    
+}
+void printTree(BTNode<int>* root){
+    if(root==NULL) return;
+    cout<<root->data<<": ";
+    if(root->left!=NULL) cout<<"L: "<<root->left->data<<" ";
+    if(root->right!=NULL) cout<<"R: "<<root->right->data<<" ";
+    cout<<endl;
+    printTree(root->left);
+    printTree(root->right);
+}
+
+void inOrder(BTNode<int>* root){
+    if(root == NULL) return;
+
+    inOrder(root->left);
+    cout << root->data << " ";
+    inOrder(root->right);
+}
+
+int main(){
+    BTNode<int>* root = takeInputLevelWise();
+    printTree(root);
+    cout << "\nInorder Traversal: ";
+    inOrder(root);
+    return 0;
+}
