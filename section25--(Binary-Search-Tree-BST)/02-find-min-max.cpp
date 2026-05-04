ec3557f514c0b9e2e9abb891b2ba431e0e97f705
Sarvesh singh <sarveshadhikari905@gmail.com>
2026-05-04 23:32:50 +0530

    find max and min val in BST

---
+++ section25--(Binary-Search-Tree-BST)/02-find-min-max.cpp
@@ -0,0 +1,83 @@
+#include<iostream>
+using namespace std;
+template<typename T>
+class BTNode {
+public:
+    T data;
+    BTNode* left;
+    BTNode* right;
+
+    BTNode(T data) {
+        this->data = data;
+        left = NULL;
+        right = NULL;
+    }
+};
+// Insert into BST
+template<typename T>
+BTNode<T>* insertBST(BTNode<T>* root, T val) {
+    if(root == NULL) {
+        return new BTNode<T>(val);
+    }
+
+    if(val < root->data) {
+        root->left = insertBST(root->left, val);
+    } else {
+        root->right = insertBST(root->right, val);
+    }
+
+    return root;
+}
+
+// Inorder Traversal (Sorted)
+template<typename T>
+void inorder(BTNode<T>* root) {
+    if(root == NULL) return;
+
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
+}
+
+// Search min in BST
+template<typename T>
+T searchMin(BTNode<T>* root) {
+    if(root==NULL) return T();
+    while(root->left != NULL){
+        root = root->left;
+    }
+    return root->data;
+}
+
+// Search max in BST
+template<typename T>
+T searchMax(BTNode<T>* root) {
+    if(root==NULL) return T();
+    while(root->right != NULL){
+        root = root->right;
+    }
+    return root->data;
+}
+
+int main() {
+    BTNode<int>* root = NULL;
+
+    int n;
+    cout << "Enter number of nodes: ";
+    cin >> n;
+
+    cout << "Enter values: ";
+    for(int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+        root = insertBST(root, x);
+    }
+
+    cout << "\nInorder Traversal (Sorted BST): ";
+    inorder(root);
+    
+    cout<<"\n Minimum value: "<<searchMin(root);
+    cout<<"\n Maximum value: "<<searchMax(root);
+
+    return 0;
+}
