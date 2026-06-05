2e2c7b43d31291f3644ecaf55f78b5ceccdffa09
Sarvesh singh <sarveshadhikari905@gmail.com>
2026-06-05 11:04:01 +0530

    k sorted array

---
+++ section27--(Priority-Queue)/04-k-sorted-array.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+
+void kSortedArray(int* input, int n, int k){
+    priority_queue<int> pq;
+    for(int i=0;i<k;i++){
+        pq.push(input[i]);
+    }
+    int s=0;
+    for(int i=k;i<n;i++){
+        input[s] = pq.top();
+        pq.pop();
+        s++;
+        pq.push(input[i]);
+    }
+    while(!pq.empty()){
+        input[s] = pq.top();
+        pq.pop();
+        s++;
+    }
+}
+
+int main(){
+    int input[] = {10,12,6,7,8,2};
+    int k = 2;
+    kSortedArray(input, 6, k);
+    for(int i=0;i<6;i++){
+        cout<<input[i]<<" ";
+    }
+    return 0;
+}
