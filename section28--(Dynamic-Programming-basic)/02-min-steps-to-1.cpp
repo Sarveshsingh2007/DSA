a49d0e758994f39c3d64d7132dfc7862732ab170
Sarvesh singh <sarveshadhikari905@gmail.com>
2026-06-07 13:05:35 +0530

    updated min steps to 1- added memorization method

--- section28--(Dynamic-Programming-basic)/02-min-steps-to-1.cpp
+++ section28--(Dynamic-Programming-basic)/02-min-steps-to-1.cpp
@@ -1,26 +1,56 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int miniSteps(int n){
+int minSteps(int n){
     if(n<=1) return 0;
-    int x = miniSteps(n-1);
+    int x = minSteps(n-1);
     int y,z;
     y = z = INT_MAX;
     if(n%2==0) {
-        y = miniSteps(n/2);
+        y = minSteps(n/2);
     }
 
     if(n%3==0){ 
-        z = miniSteps(n/3);
+        z = minSteps(n/3);
     }
     int ans = 1+ min(x, min(y,z));
     return ans;
+}
+
+int helper(int n, int *ans){
+    // base case 
+    if(n<=1) return 0;
+    if(ans[n]!=-1) return ans[n];
+    int x = helper(n-1, ans);
+    int y,z;
+    y=z=INT_MAX;
+    if(n%2==0){
+        y=helper(n/2, ans);
+    }
+    if(n%3==0){
+        z=helper(n/3, ans);
+    }
+    int output = 1+min(x,min(y,z));
+    // save ans
+    ans[n] = output;
+    return output;
+
+
+}
+
+int minSteps2(int n){
+    int *ans = new int[n+1];
+    for(int i=0;i<=n;i++){
+        ans[i] = -1;
+    }
+    return helper(n,ans);
 }
 
 int main(){
     int n;
     cout<<"Enter value of n: ";
     cin>>n;
-    cout<<"Minimum Steps: "<<miniSteps(n);
+    cout<<"Minimum Steps Brute Force: "<<minSteps(n)<<endl;
+    cout<<"Minimum Steps Memoization: "<<minSteps2(n)<<endl;
     return 0;
 }
