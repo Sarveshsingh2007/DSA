b243de6dabfa2fc47aa89850c038367ede3d114c
Sarvesh singh <sarveshadhikari905@gmail.com>
2026-06-08 12:35:53 +0530

    edit distance leetcode-72

---
+++ section28--(Dynamic-Programming-basic)/05-edit-distance-leetcode-72.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int minDistance(string s1, string s2) {
+    int m = s1.size();
+    int n = s2.size();
+
+    vector<vector<int>> dp(m + 1, vector<int>(n + 1));
+
+    for(int i = 0; i <= m; i++) {
+        for(int j = 0; j <= n; j++) {
+
+            if(i == 0) {
+                dp[i][j] = j;
+            }
+            else if(j == 0) {
+                dp[i][j] = i;
+            }
+            else if(s1[i - 1] == s2[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1];
+            }
+            else {
+                int insert = dp[i][j - 1];
+                int del = dp[i - 1][j];
+                int replace = dp[i - 1][j - 1];
+
+                dp[i][j] = 1 + min(insert, min(del, replace));
+            }
+        }
+    }
+
+    return dp[m][n];
+}
+
+int main() {
+    string s1, s2;
+
+    cout << "Enter first string: ";
+    cin >> s1;
+
+    cout << "Enter second string: ";
+    cin >> s2;
+
+    cout << "Minimum Edit Distance: " << minDistance(s1, s2) << endl;
+
+    return 0;
+}
