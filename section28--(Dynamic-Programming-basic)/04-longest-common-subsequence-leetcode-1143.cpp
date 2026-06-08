e2977dfd00705a4d1fcc15d4b1a7057c8f1e1da0
Sarvesh singh <sarveshadhikari905@gmail.com>
2026-06-08 12:35:31 +0530

    updated lcs leetcode-1143

--- section28--(Dynamic-Programming-basic)/04-longest-common-subsequence-leetcode-1143.cpp
+++ section28--(Dynamic-Programming-basic)/04-longest-common-subsequence-leetcode-1143.cpp
@@ -11,7 +11,6 @@ int longestCommonSubsequence(string s1, string s2){
 
     for(int i = 1; i <= m; i++){
         for(int j = 1; j <= n; j++){
-
             if(s1[i - 1] == s2[j - 1]){
                 dp[i][j] = 1 + dp[i - 1][j - 1];
             }
@@ -20,16 +19,12 @@ int longestCommonSubsequence(string s1, string s2){
             }
         }
     }
-
     return dp[m][n];
 }
 
 int main(){
-
     string s1 = "Apple";
     string s2 = "pple";
-
     cout << longestCommonSubsequence(s1, s2);
-
     return 0;
 }
